8df86b9e8d0fff765b710131c4fc0929467ff14f
amasclans <albert.masclans@globalzitro.com>
2018-05-15 11:44:48 +0200

    Travis trick

--- backend/src/bd.py
+++ backend/src/bd.py
@@ -1,8 +1,12 @@
 import mysql.connector
 import json
+import os
 
 def connecta():
-    with open("dades_mysql.json") as arxiu_dades:
-        dades = json.load(arxiu_dades)
-        connexio = mysql.connector.connect(user=dades["user"], password=dades["password"], host="54.38.240.12", database="signepedia", use_pure=False)
+    try:
+        with open("dades_mysql.json") as arxiu_dades:
+            dades = json.load(arxiu_dades)
+    except "FileNotFoundError":
+        dades = dict(user=os.environ["SIGNEPEDIA_MYSQL_USER"], password=os.environ["SIGNEPEDIA_MYSQL_PASSWORD"])
+    connexio = mysql.connector.connect(user=dades["user"], password=dades["password"], host="54.38.240.12", database="signepedia", use_pure=False)
     return connexio
